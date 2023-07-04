d08b39a54127af84e6ab5fe23a8ce35c64e793b3
mustafabdi100 <mustafa300abdi@gmail.com>
2023-07-04 18:32:00 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}
